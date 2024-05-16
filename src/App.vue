f721c436ca9199e1c61cc3ea0038a938848d2108
Umarjon Alimdjanov <ualimdjanov10@gmail.com>
2024-05-16 20:23:16 +0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="bg-gray-800">
+        <ul class="flex indent-10 text-white font-medium">
+            <li class="p-3 cursor-pointer hover:text-gray-500">Bosh sahifa</li>
+            <li class="p-3 cursor-pointer hover:text-gray-500">Kitoblar</li>
+            <li class="p-3 cursor-pointer hover:text-gray-500">Bestsellerlar</li>
+            <li class="p-3 cursor-pointer hover:text-gray-500">Shaxsiy kabinet</li>
+            <li class="p-3 cursor-pointer hover:text-gray-500">Kirish</li>
+            <li class="p-3 cursor-pointer hover:text-gray-500">Chiqish</li>
+        </ul>
+    </header>
+
+<!--  Main content  -->
+    <div class="grid grid-cols-12">
+
+<!--    Sidebar    -->
+        <div class="col-span-2 bg-gray-800 text-white min-h-screen border-t">
+            <ul>
+                <li class="p-3 cursor-pointer hover:text-gray-500 border-b">Klassika</li>
+                <li class="p-3 cursor-pointer hover:text-gray-500 border-b">Fantastika</li>
+                <li class="p-3 cursor-pointer hover:text-gray-500 border-b">Detektiv</li>
+                <li class="p-3 cursor-pointer hover:text-gray-500 border-b">Sarguzasht</li>
+                <li class="p-3 cursor-pointer hover:text-gray-500 border-b">Komediya</li>
+            </ul>
+        </div>
+
+<!--    Books row    -->
+        <div class="col-span-10 m-3">
+            <div class="grid grid-cols-4 gap-6">
+                <div class="col-span-1 bg-gray-100 border rounded-xl overflow-hidden shadow-lg">
+                    <img src="./assets/img/otkan_kunlar.png" alt="">
+                    <div>
+                        <h5 class="font-bold text-2xl px-2">Otkan kunlar</h5>
+                        <p class="px-2 pt-2">Moziyga qarab tutish hayrli erur</p>
+                        <a href="#" class="float-right mr-3 mb-3 py-1 px-3 bg-blue-500 text-white rounded hover:bg-blue-800">O'qish</a>
+                    </div>
+                </div>
+                <div class="col-span-1 bg-gray-100 border rounded-xl overflow-hidden shadow-lg">
+                    <img src="./assets/img/otkan_kunlar.png" alt="">
+                    <div>
+                        <h5 class="font-bold text-2xl px-2">Otkan kunlar</h5>
+                        <p class="px-2 pt-2">Moziyga qarab tutish hayrli erur</p>
+                        <a href="#" class="float-right mr-3 mb-3 py-1 px-3 bg-blue-500 text-white rounded hover:bg-blue-800">O'qish</a>
+                    </div>
+                </div>
+                <div class="col-span-1 bg-gray-100 border rounded-xl overflow-hidden shadow-lg">
+                    <img src="./assets/img/otkan_kunlar.png" alt="">
+                    <div>
+                        <h5 class="font-bold text-2xl px-2">Otkan kunlar</h5>
+                        <p class="px-2 pt-2">Moziyga qarab tutish hayrli erur</p>
+                        <a href="#" class="float-right mr-3 mb-3 py-1 px-3 bg-blue-500 text-white rounded hover:bg-blue-800">O'qish</a>
+                    </div>
+                </div>
+                <div class="col-span-1 bg-gray-100 border rounded-xl overflow-hidden shadow-lg">
+                    <img src="./assets/img/otkan_kunlar.png" alt="">
+                    <div>
+                        <h5 class="font-bold text-2xl px-2">Otkan kunlar</h5>
+                        <p class="px-2 pt-2">Moziyga qarab tutish hayrli erur</p>
+                        <a href="#" class="float-right mr-3 mb-3 py-1 px-3 bg-blue-500 text-white rounded hover:bg-blue-800">O'qish</a>
+                    </div>
+                </div>
+                <div class="col-span-1 bg-gray-100 border rounded-xl overflow-hidden shadow-lg">
+                    <img src="./assets/img/otkan_kunlar.png" alt="">
+                    <div>
+                        <h5 class="font-bold text-2xl px-2">Otkan kunlar</h5>
+                        <p class="px-2 pt-2">Moziyga qarab tutish hayrli erur</p>
+                        <a href="#" class="float-right mr-3 mb-3 py-1 px-3 bg-blue-500 text-white rounded hover:bg-blue-800">O'qish</a>
+                    </div>
+                </div>
+                <div class="col-span-1 bg-gray-100 border rounded-xl overflow-hidden shadow-lg">
+                    <img src="./assets/img/otkan_kunlar.png" alt="">
+                    <div>
+                        <h5 class="font-bold text-2xl px-2">Otkan kunlar</h5>
+                        <p class="px-2 pt-2">Moziyga qarab tutish hayrli erur</p>
+                        <a href="#" class="float-right mr-3 mb-3 py-1 px-3 bg-blue-500 text-white rounded hover:bg-blue-800">O'qish</a>
+                    </div>
+                </div>
+                <div class="col-span-1 bg-gray-100 border rounded-xl overflow-hidden shadow-lg">
+                    <img src="./assets/img/otkan_kunlar.png" alt="">
+                    <div>
+                        <h5 class="font-bold text-2xl px-2">Otkan kunlar</h5>
+                        <p class="px-2 pt-2">Moziyga qarab tutish hayrli erur</p>
+                        <a href="#" class="float-right mr-3 mb-3 py-1 px-3 bg-blue-500 text-white rounded hover:bg-blue-800">O'qish</a>
+                    </div>
+                </div>
+                <div class="col-span-1 bg-gray-100 border rounded-xl overflow-hidden shadow-lg">
+                    <img src="./assets/img/otkan_kunlar.png" alt="">
+                    <div>
+                        <h5 class="font-bold text-2xl px-2">Otkan kunlar</h5>
+                        <p class="px-2 pt-2">Moziyga qarab tutish hayrli erur</p>
+                        <a href="#" class="float-right mr-3 mb-3 py-1 px-3 bg-blue-500 text-white rounded hover:bg-blue-800">O'qish</a>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
